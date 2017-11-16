62df090bea404bdaf428f781ec4ec2f17d361812
Catherine Liaw <cat.liaw@gmail.com>
2017-11-16 00:32:28 -0800

    Adds Lob Python API to requirements; creates Lob address objects.

--- send_to_legislator.py
+++ send_to_legislator.py
@@ -5,11 +5,11 @@
 
 import sys
 import os
-
+import lob
 from apiclient.discovery import build
 
 google_civic_key = os.environ['GOOGLE_CIVIC_KEY']
-lob_test_api_key = os.environ['LOB_TEST_API_KEY']
+lob.api_key = os.environ['LOB_TEST_API_KEY']
 
 lines = []
 sender_info = {}
@@ -23,13 +23,12 @@ input_key_legend = {
     "From Zip Code": "from_zip_code",
     "Message": "message"
 }
-
 governor_info = {}
 
-#########################################################################
-# This command line tool allows one argument - a text file that is opened
-# and read. Each line then parsed into a Python dictionary (sender_info)
-#########################################################################
+###########################################################################
+# This command line tool allows one argument - a text file that is opened #
+# and read. Each line then parsed into a Python dictionary (sender_info)  #
+###########################################################################
 
 # open and read input file
 with open(sys.argv[1], "r") as f:
@@ -54,6 +53,7 @@ for line in lines:
 
     sender_info[input_key] = input_val
 print sender_info
+print sender_info['message']
 
 # new address string to input into Google Civic API as parameter later
 sender_info['complete_address'] = "%s %s %s %s %s" % (sender_info["from_address_line_1"],
@@ -64,10 +64,10 @@ sender_info['complete_address'] = "%s %s %s %s %s" % (sender_info["from_address_
 print sender_info['complete_address']
 
 
-#################################################################
-# Using the Google Civic API and sender's address, find the local
-# legislator info (specified to state governor in this function).
-#################################################################
+###################################################################
+# Using the Google Civic API and sender's address, find the local #
+# legislator info (specified to state governor in this function). #
+###################################################################
 
 # Create a service object for the correct API, version, authentication key
 service = build('civicinfo', 'v2', developerKey=google_civic_key)
@@ -92,10 +92,51 @@ for key, value in civic_response['officials'][0]['address'][0].iteritems():
 
 print governor_info
 
-########################################################################
-# Using Lob API, create a letter with sender's name/address, 
-# governor's name/address, and message to governor (500 character limit)
-########################################################################
+##########################################################################
+# Using Lob API, create a letter with sender's name/address,             # 
+# governor's name/address, and message to governor (500 character limit) #
+##########################################################################
 
+# Create a Lob Address object for the sender
+sender_address = lob.Address.create(
+    name=sender_info["from_name"],
+    address_line1=sender_info["from_address_line_1"],
+    address_line2=sender_info["from_address_line_2"],
+    address_city=sender_info["from_city"],
+    address_state=sender_info["from_state"],
+    address_zip=sender_info["from_zip_code"]
+)
+print sender_address
 
+# Create a Lob Address object for the governor (legislator)
+governor_address = lob.Address.create(
+    name=governor_info["name"],
+    address_line1=governor_info["line1"],
+    address_line2=governor_info["line2"],
+    address_city=governor_info["city"],
+    address_state=governor_info["state"],
+    address_zip=governor_info["zip"]
+)
+print governor_address
 
+# letter_to_governor = lob.Letter.create(
+#     description='Letter to Governor',
+#     to_address=governor_address,
+#     from_address=sender_address,
+#     file="""
+#       <html>
+#         <head>
+#           <style>
+#             @font-face {
+#               font-family: 'Loved by the King';
+#               src: url('https://s3-us-west-2.amazonaws.com/lob-assets/LovedbytheKing.ttf');
+#             }
+#           </style>
+#         </head>
+#         <body><h1>Special offer for {{message}}</h1></body>
+#       </html>""",
+#     merge_variables={
+#         'message': sender_info['message']
+#     },
+#     color=False
+# )
